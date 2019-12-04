3e61d04fcedf97a1221e9c6f501362e36c3860d0
Stefan Rothmajer <stefan.rothmajer@wirecard.com>
2019-12-04 14:43:23 +0100

    title image loading fix

--- components/title_photo/TheTitlePhoto.vue
+++ components/title_photo/TheTitlePhoto.vue
@@ -1,9 +1,11 @@
 <template>
-  <div v-if="titleData" class="w-screen sm:h-550 h-80 relative">
-    <img :src="titleData.foto" class="h-full w-full object-cover block" alt="Titulna fotka">
-    <div v-html="titleData.nadpis" class="absolute bottom-100 sm:bottom-180 right-10 sm:right-40 text-white font-worksans text-3xl sm:text-6xl" />
-    <div class="absolute font-worksansLight text-center hidden sm:bottom-100 sm:block text-white w-80 right-10 sm:right-40">
-      {{ titleData.podnadpis }}
+  <div class="w-screen sm:h-550 h-80">
+    <div v-if="titleData" class="w-screen sm:h-550 h-80 relative">
+      <img :src="titleData.foto" width="auto" height="320" class="h-full w-full object-cover block" alt="Titulna fotka">
+      <div v-html="titleData.nadpis" class="absolute bottom-100 sm:bottom-180 right-10 sm:right-40 text-white font-worksans text-3xl sm:text-6xl" />
+      <div class="absolute font-worksansLight text-center hidden sm:bottom-100 sm:block text-white w-80 right-10 sm:right-40">
+        {{ titleData.podnadpis }}
+      </div>
     </div>
   </div>
 </template>
