b4492784e91bc6c0377cf0f49aa81377de00d257
Stefan Rothmajer <stefan.rothmajer@wirecard.com>
2019-12-07 14:11:18 +0100

    fix leaflet css nr 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   ** Global CSS
   */
   css: [
-    '~/node_modules/leaflet/dist/leaflet.css'
+    'leaflet/dist/leaflet.css'
   ],
   /*
   ** Plugins to load before mounting the App
