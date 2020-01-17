222378d9fab62bd7a170ee45e535027294c141fb
Rothmajer <stefan.rothmajer@wirecard.com>
2020-01-17 06:49:42 +0100

    quote font fix

--- components/title_banner/TheTitleBanner.vue
+++ components/title_banner/TheTitleBanner.vue
@@ -3,8 +3,8 @@
     <div v-if="home" class="w-full mb-8 text-center text-gray font-worksans text-sm sm:text-lg">
       {{ home.page_description }}
     </div>
-    <div class="flex flex-col-reverse lg:flex-row justify-between">
-      <div v-if="home" class="mt-8 flex-1 font-satisfy text-lightGray text-3xl font-medium md:w-full xl:w-2/5 text-center self-center">
+    <div class="flex flex-col lg:flex-row justify-between">
+      <div v-if="home" class="my-8 flex-1 font-satisfy text-lightGray text-xl sm:text-3xl font-medium md:w-full xl:w-2/5 text-center self-center">
         {{ home.quote_text }}
       </div>
       <img v-if="home" :src="home.quote_image" alt="book">
